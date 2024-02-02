642cc22b033d0e23f58c74f43d6c46bf60ec3a08
Tanush Chauhan <tanushchauhan07@gmail.com>
2024-02-02 01:53:50 -0600

    Fixed form submissions.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -17,22 +17,19 @@ function Contact() {
             Have questions? Fill out this quick form and we will be in touch in
             no time!
           </p>
-          <form
-            className="space-y-8"
-            name="contact"
-            method="POST"
-            data-netlify="true"
-          >
+          <form className="space-y-8" name="contact" method="post">
+            <input type="hidden" name="form-name" value="contact" />
             <div>
               <label
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                name="email"
+                htmlFor="email"
               >
                 Your email
               </label>
               <input
                 type="email"
                 id="email"
+                name="email"
                 className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                 placeholder="Your Email Here"
                 required
@@ -41,13 +38,14 @@ function Contact() {
             <div>
               <label
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                name="subject"
+                htmlFor="subject"
               >
                 Subject
               </label>
               <input
                 type="text"
                 id="subject"
+                name="subject"
                 className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                 placeholder="Let us know how we can help you"
                 required
@@ -56,12 +54,13 @@ function Contact() {
             <div className="sm:col-span-2">
               <label
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
-                name="message"
+                htmlFor="message"
               >
                 Your message
               </label>
               <textarea
                 id="message"
+                name="message"
                 rows="6"
                 className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="Leave a comment..."
